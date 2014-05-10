f757176a150d021833154ac7e3fe968ddedafc52
Liam <liamw950@gmail.com>
2014-05-10 16:17:52 +0930

    Add comments about verifying XML, I don't know the speed implications but it shouldn't be much at all

--- src/Bomcheck.hs
+++ src/Bomcheck.hs
@@ -2,4 +2,6 @@ module Bomcheck.Bomcheck(bomcheck) where
 
 -- Links the two together and gives us a higher level API
 
+-- Call verifyXML only if needed
+
 bomcheck = "Blank"
--- src/Process.hs
+++ src/Process.hs
@@ -2,5 +2,6 @@ module Bomcheck.Process(process) where
 
 -- changes from XML into something Haskell can use
 -- Needs what data to show
+-- Also verifys xml if asked, benchmark this
 
 process = "Change the data!"
