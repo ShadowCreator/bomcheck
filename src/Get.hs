e754253a824faa48266aeaa5861bf58843d321e3
Liam <liamw950@gmail.com>
2014-05-12 12:21:35 +0930

    Rename

---
+++ src/Get.hs
@@ -0,0 +1,6 @@
+module Bomcheck.Get(get) where
+
+-- Needs location
+-- Also export two functions, one to get main xml and one to get s
+
+get = "GET THE DATA!"
--- src/GetData.hs
+++
@@ -1,6 +0,0 @@
-module Bomcheck.GetData(getData) where
-
--- Needs location
--- Also export two functions, one to get main xml and one to get schema
-
-getData = "GET THE DATA!"
