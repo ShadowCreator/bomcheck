795f8a56a85f81aca4a0e299ddb3a9ff0707c66b
Liam <liamw950@gmail.com>
2014-05-14 13:24:33 +0930

    Revert "Git went odd and says this is modified"
    This reverts commit b08859b62a0d268fae2fbeae63f73dac98186da1.

--- src/Location.hs
+++ src/Location.hs
@@ -1,4 +1,10 @@
 module Bomcheck.Location where
 
--- What are the applicable locations?
-data Location
+-- There might be a way to load this info from a data file at build time? TH?
+data Location = Adelaide | Sydney
+
+location :: Location -> String
+location Adelaide = "IDS60901"
+-- Rest of Adelaide area
+location Sydney = "IDN60900"
+-- Rest of areas
