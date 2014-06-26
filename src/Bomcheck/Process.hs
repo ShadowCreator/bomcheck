b6285841b2656bdfd7275df307fa67058b1d925c
Liam <liamw950@gmail.com>
2014-06-26 11:13:48 +0930

    Basic Process

--- src/Bomcheck/Process.hs
+++ src/Bomcheck/Process.hs
@@ -4,4 +4,23 @@ module Bomcheck.Process(process) where
 -- Needs what data to show and location
 -- Also verifys xml if asked, benchmark this
 
-process = "Change the data!"
+-- What to import
+import Location
+import DataToDisplay
+
+--import tagsoup
+
+
+process :: IO String -> DataToDisplay -> Location -> Either String ()
+--process :: String -> DataToDisplay -> Location -> IO (String)
+process xml dtd loc = do
+  --Check if it's XML returned
+  
+  -- readXML
+ 
+  -- extract section with bom_id l
+  
+  -- extract the data_to_display
+  
+  -- return an IO string or failure
+  return ()
